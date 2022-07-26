dc8fedac2df981e02b5bbbf904b7a88faaefa1a3
Apethesis <apethesis@gmail.com>
2022-07-26 20:29:23 +0200

    Update bios.lua

--- bios.lua
+++ bios.lua
@@ -4,7 +4,7 @@
 -- available to the BIOS or any APIs. All APIs load this using dofile, but that
 -- has not been defined at this point.
 local expect
-
+table.foreachi = nil
 if not table.unpack then table.unpack = unpack end
 if not table.pack then table.pack = function( ... ) return { n = select( "#", ... ), ... } end end
 if not load then
